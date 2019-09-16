3c5bf50cfe75370bacc94f5645bd481a73426803
GanZiPokPung <sb9212@kpu.ac.kr>
2019-09-16 11:58:02 +0900

    190916
    1. modify define to const global value

--- Project_NGP/Project_NGP/Constant.h
+++ Project_NGP/Project_NGP/Constant.h
@@ -1,2 +1,9 @@
 #pragma once
 
+const UINT WINSIZE_X = 1024;
+const UINT WINSIZE_Y = 768;
+
+const float PI = 3.141592f;
+const float GRAVITY_ACC = 9.8f;
+
+const UINT KEY_MAX = 256;
--- Project_NGP/Project_NGP/Define.h
+++ Project_NGP/Project_NGP/Define.h
@@ -1,14 +1,6 @@
 #pragma once
 
-#define WINSIZE_X 1024
-#define WINSIZE_Y 768
-
 #define PURE = 0
-
-#define PI 3.141592f
-#define GRAVITY_ACC 9.8f
-
-#define KEY_MAX 256
 
 /* Singleton Pattern */
 #define DECLARE_SINGLETON(ClassName)	\
